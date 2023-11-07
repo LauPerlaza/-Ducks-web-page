27e59ff21a5e9f1674ef6589df422cc3c89f273b
laura perlaza <117466164+LauPerlaza@users.noreply.github.com>
2023-11-07 16:59:23 -0500

    correccion del path del backend

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
 
   backend "s3" {
     bucket = "web-ducks-server-develop"
-    key = "path/to/my/key"
+    key = "web-ducks-server-develop"
     region = "us-east-1"
     dynamodb_table = "dynamodb-ducks-develop"
     encrypt = true
